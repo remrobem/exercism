d90437cd2fa6ed1306e732ea2a05eeb2ddbaf171
Rob Martin <rob.martin.5758@gmail.com>
2019-11-24 20:49:34 -0500

    complete queen-attack

--- javascript/queen-attack/queen-attack.js
+++ javascript/queen-attack/queen-attack.js
@@ -1,41 +1,51 @@
-//
-// This is only a SKELETON file for the 'Queen Attack' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class QueenAttack {
-    constructor( { white = [0,3], black = [7,3] }) {
+    constructor({ white, black } = { white: [0, 3], black: [7, 3] }) {
         this.white = white;
         this.black = black;
-        console.log('this.white: ', this.white)
-        console.log('this.black: ', this.black)
+        [this.whiteRow, this.whiteCol, this.blackRow, this.blackCol] = [...this.white, ...this.black];
+        /* white and black cannot occupy same cell */
+        if (this.whiteRow == this.blackRow && this.whiteCol == this.blackCol) {
+            throw new Error("Queens cannot share the same space");
+        }
     }
 
     toString() {
         let board = ['_ _ _ _ _ _ _ _',
-      '_ _ _ _ _ _ _ _',
-      '_ _ _ _ _ _ _ _',
-      '_ _ _ _ _ _ _ _',
-      '_ _ _ _ _ _ _ _',
-      '_ _ _ _ _ _ _ _',
-      '_ _ _ _ _ _ _ _',
-      '_ _ _ _ _ _ _ _\n']
+            '_ _ _ _ _ _ _ _',
+            '_ _ _ _ _ _ _ _',
+            '_ _ _ _ _ _ _ _',
+            '_ _ _ _ _ _ _ _',
+            '_ _ _ _ _ _ _ _',
+            '_ _ _ _ _ _ _ _',
+            '_ _ _ _ _ _ _ _'];
 
-      board = this.setColor(board, this.white, 'W');
-      board = this.setColor(board, this.black, 'B');
-      return board.join('\n');
+        board = this.setCellColor(board, this.white, 'W');
+        board = this.setCellColor(board, this.black, 'B');
+        /* assumes board is square */
+        /* newline required in last cell */
+        board[board.length -1 ,board.length -1] += '\n';
+        return board.join('\n');
 
+    }
+
+    setCellColor(board, cell, value) {
+        let row = board[cell[0]].split(' ');
+        row[cell[1]] = value;
+        row = row.join(' ');
+        board[cell[0]] = row;
+        return board;
     }
 
     canAttack() {
-        throw new Error("Remove this statement and implement this function");
-    }
+        /* diagonal check - an equal absolute diff between row and col means attack is allowed */
+        if (Math.abs(this.whiteRow - this.blackRow) === Math.abs(this.whiteCol - this.blackCol)) {
+            return true;
+        }
+        /* same row or column allows attack */
+        if (this.whiteRow == this.blackRow || this.whiteCol == this.blackCol) {
+            return true;
+        }
 
-    setColor (board, cell, value) {
-      let row = board[cell[0]].split(' ');
-      row[cell[1]] = value;
-      row = row.join(' ');
-      board[cell[0]] = row;
-      return board;
+        return false;
     }
 }
