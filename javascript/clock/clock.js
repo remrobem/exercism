ba4251c2e42dcfd2f05dbd841e442f7af94eadc6
Rob Martin <rob.martin.5758@gmail.com>
2019-11-26 17:42:55 -0500

    complete

--- javascript/clock/clock.js
+++ javascript/clock/clock.js
@@ -1,42 +1,33 @@
-
-let originalWorkDate = null;
-
 export class Clock {
   constructor(hours, minutes) {
     this.hours = hours || 0;
     this.minutes = minutes || 0;
     this.workDate = new Date(2000, 0, 1, this.hours, this.minutes);
-
-    if (!originalWorkDate) {
-      originalWorkDate = this.workDate;
-    }
   }
 
   toString() {
-    return this.formatedTime();
+    return this.formatedTime(this.workDate);
   }
 
   plus(minutes) {
     this.workDate.setMinutes(this.workDate.getMinutes() + minutes);
-    return this.formatedTime();
+    return this.formatedTime(this.workDate);
 
   }
 
   minus(minutes) {
     this.workDate.setMinutes(this.workDate.getMinutes() - minutes);
-    return this.formatedTime();
+    return this.formatedTime(this.workDate);
   }
+
 // arg format:  Clock { hours: 15, minutes: 37, workDate: 2000-01-01T20:37:00.000Z }
   equals(arg) {
-    console.log('arg1 clock date: ', arg.workDate)
-    // console.log('Equal Orig: ', originalWorkDate);
-    console.log('Equal work: ', this.workDate)
-    return arg.workDate.getTime() === this.workDate.getTime() ? true : false;
+    return this.formatedTime(arg.workDate) === this.formatedTime(this.workDate);
   }
 
   // format time for return
-  formatedTime() {
-    return (`${this.padZero(this.workDate.getHours())}:${this.padZero(this.workDate.getMinutes())}`)
+  formatedTime(date) {
+    return (`${this.padZero(date.getHours())}:${this.padZero(date.getMinutes())}`)
   }
 
   // add leading zero when needed
