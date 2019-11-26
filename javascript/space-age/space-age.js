281c6ed63c80735302c1130884d24ecda68905fd
Rob Martin <rob.martin.5758@gmail.com>
2019-11-26 14:25:36 -0500

    refactor - move const , use earth secs in formula

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,23 +1,23 @@
+const EARTH_SECONDS_PER_YEAR = 31557600;
+const ORBIT_IN_EARTH_YEARS = {
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  earth: 1.0,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132,
+};
+
 export const age = (planet, seconds) => {
-  const earthSecondsPerYear = 31557600;
-  const planetEarthSeconds = {
-    mercury: 0.2408467 * earthSecondsPerYear,
-    venus: 0.61519726 * earthSecondsPerYear,
-    earth: 1.0 * earthSecondsPerYear,
-    mars: 1.8808158 * earthSecondsPerYear,
-    jupiter: 11.862615 * earthSecondsPerYear,
-    saturn: 29.447498 * earthSecondsPerYear,
-    uranus: 84.016846 * earthSecondsPerYear,
-    neptune: 164.79132 * earthSecondsPerYear,
-  };
   // validate planet argument
-  if (!Object.hasOwnProperty.call(planetEarthSeconds, planet)) {
+  if (!Object.hasOwnProperty.call(ORBIT_IN_EARTH_YEARS, planet)) {
     throw new Error(`Invalid planet: ${planet}`)
   }
   // validate seconds argument
   if (typeof seconds != 'number') {
     throw new Error(`Invalid seconds: ${seconds}`)
   }
-
-  return parseFloat((seconds / planetEarthSeconds[planet]).toFixed(2));
+  return Number((seconds / (ORBIT_IN_EARTH_YEARS[planet] * EARTH_SECONDS_PER_YEAR)).toFixed(2));
 };
