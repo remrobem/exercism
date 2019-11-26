ae3b158e70b7bd0cf0fb3f764370a25452f889c8
Rob Martin <rob.martin.5758@gmail.com>
2019-11-26 16:36:40 -0500

    clock  all but equal methoids working

--- javascript/clock/clock.js
+++ javascript/clock/clock.js
@@ -1,26 +1,46 @@
-//
-// This is only a SKELETON file for the 'Clock' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+
+let originalWorkDate = null;
 
 export class Clock {
-  constructor() {
-    throw new Error('Remove this statement and implement this function');
+  constructor(hours, minutes) {
+    this.hours = hours || 0;
+    this.minutes = minutes || 0;
+    this.workDate = new Date(2000, 0, 1, this.hours, this.minutes);
+
+    if (!originalWorkDate) {
+      originalWorkDate = this.workDate;
+    }
   }
 
   toString() {
-    throw new Error('Remove this statement and implement this function');
+    return this.formatedTime();
   }
 
-  plus() {
-    throw new Error('Remove this statement and implement this function');
+  plus(minutes) {
+    this.workDate.setMinutes(this.workDate.getMinutes() + minutes);
+    return this.formatedTime();
+
   }
 
-  minus() {
-    throw new Error('Remove this statement and implement this function');
+  minus(minutes) {
+    this.workDate.setMinutes(this.workDate.getMinutes() - minutes);
+    return this.formatedTime();
+  }
+// arg format:  Clock { hours: 15, minutes: 37, workDate: 2000-01-01T20:37:00.000Z }
+  equals(arg) {
+    console.log('arg1 clock date: ', arg.workDate)
+    // console.log('Equal Orig: ', originalWorkDate);
+    console.log('Equal work: ', this.workDate)
+    return arg.workDate.getTime() === this.workDate.getTime() ? true : false;
   }
 
-  equals() {
-    throw new Error('Remove this statement and implement this function');
+  // format time for return
+  formatedTime() {
+    return (`${this.padZero(this.workDate.getHours())}:${this.padZero(this.workDate.getMinutes())}`)
+  }
+
+  // add leading zero when needed
+  padZero(number) {
+    return (number < 10 ? '0' : '') + number
   }
 }
