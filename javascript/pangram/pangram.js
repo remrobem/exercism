87d87d1bda16889bd1189cca456aa9f705abc8f8
Rob Martin <rob.martin.5758@gmail.com>
2019-11-26 19:14:40 -0500

    ternary check on return not needed

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -7,19 +7,19 @@ export const isPangram = (string) => {
 string = string.toLowerCase();
 
 // filter will search for all letters, even if one has alreay been found missing
-// let missing = ALPHABET.filter( (letter) => {
-//   return !string.includes(letter)
-// })
+let missing = ALPHABET.filter( (letter) => {
+  return !string.includes(letter)
+})
 
-// return missing.length == 0 ? true : false
+return missing.length == 0;
 
 // for loop will stop on first missing letter
-for (let i=0; i < ALPHABET.length - 1; i++) {
-  if (!string.includes(ALPHABET[i])) {
-    return false;
-  }
-}
+// for (let i=0; i < ALPHABET.length - 1; i++) {
+//   if (!string.includes(ALPHABET[i])) {
+//     return false;
+//   }
+// }
 
-return true;
+// return true;
 
 };
