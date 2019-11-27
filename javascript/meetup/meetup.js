fa12ad89d1733541b3ca72e28e73194ab6df8422
Rob Martin <rob.martin.5758@gmail.com>
2019-11-27 17:37:10 -0500

    testing and fixing

--- javascript/meetup/meetup.js
+++ javascript/meetup/meetup.js
@@ -1,80 +1,84 @@
-import { forInStatement } from "@babel/types";
-
-//
-// This is only a SKELETON file for the 'Meetup' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-// const DAYS_OF_WEEK = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const DAYS_OF_WEEK = { Sunday: 0, Monday: 1, Tuesday: 2, Wednesday: 3, Thursday: 4, Friday: 5, Saturday: 6 };
-
 
 export const meetupDay = (p_year, p_month, p_day, p_qualifier) => {
 
   const first = () => {
-    console.log('first');
+    return getMeetingDate(1);
   }
 
   const second = () => {
-    console.log('second');
-
+    return getMeetingDate(7);
   }
+
   const third = () => {
-    console.log('third');
-
+    return getMeetingDate(13);
   }
+
   const fourth = () => {
-    console.log('fourth');
-
+    return getMeetingDate(20);
   }
+
   const fifth = () => {
-    console.log('fifth');
-
+    const date = getMeetingDate(27);
+    if (date.getMonth()!= p_month) {
+        throw new Error(`There is no fifth ${p_day} for ${p_month} ${p_year}`);
+    }
+    return date;
   }
+
   const last = () => {
     console.log('last');
+    const fourth =  getMeetingDate(20);
+    const fifth = getMeetingDate(27);
+    console.log('fourth: ', fourth);
+    console.log('fifth: ', fifth)
+    return fifth.getMonth() != p_month ? fourth : fifth;
+
 
   }
+
   const teenth = () => {
-    console.log('teenth');
     const date13th = new Date(p_year, p_month, 13)
     const dayOfWeek13th = date13th.getDay();
-    let newDay;
+    let newDate;
 
-    newDay = day < dayOfWeek13th
-      ? 20 + (day - dayOfWeek13th)
-      : 13 + (day - dayOfWeek13th);
+    newDate = meetingDayOfWeek < dayOfWeek13th
+      ? 20 + (meetingDayOfWeek - dayOfWeek13th)
+      : 13 + (meetingDayOfWeek - dayOfWeek13th);
 
-    console.log(p_year, p_month, newDay);
-
-    return new Date(p_year, p_month, newDay)
+    return new Date(p_year, p_month, newDate)
 
   }
 
+  const getMeetingDate = (startDate) => {
+
+    // let newDate = '';
+    let meetingDate = '';
+    const startOfMonth = new Date(p_year, p_month, startDate);
+    const startDayOfWeek = startOfMonth.getDay();
+    const diffDayOfWeek = meetingDayOfWeek - startDayOfWeek;
+    console.log('1 - startofmonth: ', startOfMonth);
+
+    if (diffDayOfWeek >= 0) {
+      startOfMonth.setDate(startOfMonth.getDate() + startDate + meetingDayOfWeek);
+      // meetingDate = new Date(p_year, p_month, (startDate + meetingDayOfWeek));
+    } else {
+      startOfMonth.setDate(startOfMonth.getDate() + startDate + 7 + diffDayOfWeek);
+      // meetingDate = new Date(p_year, p_month, (startDate + 7 + diffDayOfWeek));
+    }
+// console.log('meetingDate: ', meetingDate)
+    // newDate = meetingDate.getDate();
+    // return new Date(p_year, p_month, newDate);
+    // return meetingDate;
+console.log('2 - startofmonth: ', startOfMonth);
+
+    return startOfMonth;
+  }
+
+
   const QUALIFIER_FUNC = { '1st': first, '2nd': second, '3rd': third, '4th': fourth, '5th': fifth, 'last': last, 'teenth': teenth };
-  const day = DAYS_OF_WEEK[p_day];
-
-  // if (p_qualifier === 'teenth') {
-  //   const date13th = new Date(p_year, p_month, 13)
-  //   const dayOfWeek13th = date13th.getDay();
-  //   let newDay;
-
-  //   newDay = day < dayOfWeek13th
-  //   ? 20 + (day - dayOfWeek13th)
-  //   : 13 + (day - dayOfWeek13th);
-
-  //   return new Date(p_year, p_month, newDay)
-  // }
-
-  const func = { '1st': 'first' }
-  let test = () => console.log('test ran');
-  let testData = { first: test }
-  // testData.first();
-  // console.log(func[p_qualifier]);
-  // testData[func[p_qualifier]]();
-
+  const meetingDayOfWeek = DAYS_OF_WEEK[p_day];
 
   return QUALIFIER_FUNC[p_qualifier]();
-
-
   // throw new Error("Remove this statement and implement this function");
 }
