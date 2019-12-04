99bb1ccd85a854ae5a48da6ac410331f5f163352
Rob Martin <rob.martin.5758@gmail.com>
2019-12-04 16:04:20 -0500

    refactor

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -4,24 +4,29 @@ export class Matrix {
   }
 
   get rows() {
-    return this.buildRows(this.string);
+    return this._rows ? this._rows : this.buildRows(this.string);
   }
 
   get columns() {
-    let rows = this.buildRows(this.string);
-    // transpose rows so columns are now rows
-    return rows[0].map((col, i) => rows.map(row => row[i]));
+    return this._columns ? this._columns : this.buildColumns();
   }
 
   buildRows(string) {
-
     // 2 dim array of the matrix string
     let rows = [];
     string.split('\n').map((row) => {
       rows.push((row.split(' ')));
     })
     // convert strings to numbers in the inner array
-   return rows.map((row) => row.map((number) => Number(number)));
+    return rows.map((row) => row.map(Number));
+  }
+
+  buildColumns() {
+    // rows array required to determine columns
+    this._rows = this._rows ? this._rows : this.buildRows(this.string);
+
+    // transpose rows so columns are now rows
+    return this._rows[0].map((col, i) => this._rows.map(row => row[i]));
   }
 
 }
