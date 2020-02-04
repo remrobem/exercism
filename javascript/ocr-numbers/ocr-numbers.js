9753aa73c5e8df0a1f02e7a45df75076d1d158cb
Rob Martin <rob.martin.5758@gmail.com>
2020-02-04 17:19:02 -0500

    completed

--- javascript/ocr-numbers/ocr-numbers.js
+++ javascript/ocr-numbers/ocr-numbers.js
@@ -1,28 +1,8 @@
-//
-// This is only a SKELETON file for the 'OCR Numbers' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+"use strict";
 
-//   '    _  _     _  _  _  _  _  _ \n'
-// + '  | _| _||_||_ |_   ||_||_|| |\n'
-// + '  ||_  _|  | _||_|  ||_| _||_|\n'
-// + '                              ',
-
-// '    _  _     _  _  _  _  _  _ \n' + '  | _| _||_||_ |_   ||_||_|| |\n' + '  ||_  _|  | _||_|  ||_| _||_|\n' + '                              ',
-const test =
-  '    _  _     _  _  _  _  _  _ '
-  + '  | _| _||_||_ |_   ||_||_|| |'
-  + '  ||_  _|  | _||_|  ||_| _||_|'
-  + '                              ';
-
-// const test =  ' _ ' + '| |' + '|_|' + '   ';
-//const test1 = '    _  _     _  _  _  _  _  _   | _| _||_||_ |_   ||_||_|| |  ||_  _|  | _||_|  ||_| _||_|                              ';
-
-
-// const convert = (string) => {
 export const convert = (string) => {
 
-  // array of all digits used to compare to input
+  // array of all digits, used to compare to input
   const ocrs = [
     // zero
     ' _ ' + '| |' + '|_|' + '   ',
@@ -45,87 +25,59 @@ export const convert = (string) => {
     // nine   
     ' _ ' + '|_|' + ' _|' + '   ',
   ]
-  // let comma = '         \n';
-  // const commaLocations = [];
 
-  // let findComma = string.indexOf(comma);
-  // // console.log('1 findComma: ', findComma)
-  // while (findComma >= 0) {
-  //   commaLocations.push(findComma);
-  //   findComma = string.indexOf(comma, findComma + 1)
-  // }
-
-  // for (let i = 0; i < string.length; i++) {
-  //   console.log(string.indexOf(comma, i))
-  //   if (string.indexOf(comma, i) >= 0) {
-  //     commaLocations.push[i];
-  //   }
-  // }
-  // console.log('commaLocations: ', commaLocations);
-  // remove new line characters from input
-  // string = string.replace(comma, ',');
-
-
-
-  // a digit is a 3x4 grid (12 values)
-  // make sure input is a multiple of 12
-  // if (ocrInput.length % 12 != 0) {
-  //   throw new Error("Malformed input. Not divisible by 12");
-  // }
-
-  // how many digits exist in the input
-  // const digitCount = ocrInput.length / 12;
-  // const firstNewLine = string.indexOf('\n');
-  // const digitsPerLine = firstNewLine / 3;
-  // const lineLength = digitsPerLine * 12;
-
-  // const ocrString = string.replace(/\n/g, '');
   let ocrString = string;
-  // console.log('firstNewLine: ', firstNewLine, digitsPerLine)
 
   let ocrArray = [];
   let ocrChar = '';
 
   while (ocrString.length > 0) {
-    const firstNewLine = ocrString.indexOf('\n') >= 0 ? ocrString.indexOf('\n') : ocrString.length;
-    const digitCount = firstNewLine / 3;
-    const lineLength = digitCount * 12;
-    // const ocrHold = ocrString.replace(/\n/g, '');
-    const ocrInput = ocrString.replace(/\n/g, '').substring(0, lineLength);
-
-    ocrString = ocrString.substring(firstNewLine + 1);
+    // index for newline string
+    const newLineIndex = ocrString.indexOf('\n');
+    // last line does not have \n newline
+    const lineLength = newLineIndex >= 0 ? newLineIndex : ocrString.length;
+    // each digit is 3 positions in a line
+    const digitCount = lineLength / 3;
+    const digitsLength = digitCount * 12;
+    // one set of digits when seperated in the input string 
+    const ocrInput = ocrString.replace(/\n/g, '').substring(0, digitsLength);
+    // remove characters just processed from the string for the next iteration
+    ocrString = ocrString.substring((lineLength + 1) * 4);
 
     // number of char to skip to find next row for a digit 
-    let skip = (digitCount - 1) * 3;
+    let skip = (digitCount * 3) - 2;
 
     // one time for each digit in input
     for (let count = 0; count <= digitCount - 1; count++) {
-      // get the characters for each digit in the input building an array for each digit
-      for (let column = count * 3; column < ocrInput.length;) {
-        ocrChar = ocrChar.concat(ocrInput.charAt(column))
-        column++;
-        ocrChar = ocrChar.concat(ocrInput.charAt(column));
-        column++;
-        ocrChar = ocrChar.concat(ocrInput.charAt(column));
-        column = column + skip;
+      // get the characters for each digit in the input,  build an array for each digit
+      // a digit consumes 3 columns and 4 rows, the values for these rows are seperated by 
+      // values for other digits
+      // so this gets 3 values (columns) skips some values based on number of digits, gets next 3 values
+      // until end of array (should be 4 times).
+      let counter = 1;
+      for (let position = count * 3; counter <= 4;) {
+        ocrChar = ocrChar.concat(ocrInput.charAt(position))
+        position++;
+        ocrChar = ocrChar.concat(ocrInput.charAt(position));
+        position++;
+        ocrChar = ocrChar.concat(ocrInput.charAt(position));
+        position += skip;
+        counter++;
       }
       ocrArray.push(ocrChar);
       ocrChar = '';
     }
-
-
+    // seperate with comma when input has sets of numbers
+    if (ocrString.length > 0) {
+      ocrArray.push(',');
+    }
   }
-  // splice return ????
-  console.log('ocrArray: ', ocrArray)
 
-  // look up the input digit value and return it
+  // look up the input digit value and return it or the comma seperator
   return ocrArray.map(ocr => {
-    const ocrValue = ocrs.indexOf(ocr);
-    return ocrValue >= 0 ? ocrValue : '?';
-  })
-    .join('');
+    return ocr == ',' ? ocr
+      : ocrs.indexOf(ocr) >= 0 ? ocrs.indexOf(ocr)
+        : '?';
+  }).join('');
 
 };
-
-
-// console.log(convert(test))
