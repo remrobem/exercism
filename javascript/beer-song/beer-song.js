852c5977e99ff9818aac49b5441c91182cf57144
Rob Martin <rob.martin.5758@gmail.com>
2020-02-05 11:57:26 -0500

    complete

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -1,40 +1,31 @@
-//
-// This is only a SKELETON file for the 'Beer Song' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-
 export const recite = (initialBottlesCount, takeDownCount) => {
 
   let onWall = initialBottlesCount;
-  let leftOnWall = onWall - 1;
-  let onWallplural = 's';
-  let leftOnWallPlural = 's';
-  let response = [];
+  const response = [];
 
-  const mainVerse = `
-${onWall} bottle${onWallplural} of beer on the wall, ${onWall} bottle${onWallplural} of beer.
-Take one down and pass it around, ${leftOnWall} bottle${leftOnWallPlural} of beer on the wall.`
-  const lastVerse = `
-No more bottles of beer on the wall, no more bottles of beer.
-Go to the store and buy some more, 99 bottles of beer on the wall.`
+  const lastVerse1 = 'No more bottles of beer on the wall, no more bottles of beer.'
+  const lastVerse2 = 'Go to the store and buy some more, 99 bottles of beer on the wall.'
 
-  for (let count = 0; count < takeDownCount; count++) {
-    // leftOnWall = onWall - 1;
-    if (leftOnWall == 0) {
-      response.push(lastVerse);
-      break;
+  for (let count = 0; count < takeDownCount; count++, onWall--) {
+
+    if (onWall == 0) {
+      response.push(lastVerse1, lastVerse2);
+      return response;
     }
-    onWallplural = onWall > 1 ? 's' : '';
-    leftOnWallPlural = leftOnWall > 1 ? 's' : '';
-    response.push(mainVerse);
-    response.push('')
-    onWall -= 1;
-    leftOnWall -= 1;
-    // console.log(mainVerse);
+   
+    // used a variable becuse it is referenced multiple time in template literal
+    const onWallplural = onWall > 1 ? 's' : '';
+
+    // these template literals need to the defined in for loop so the vaaiables get recalculated
+    let mainVerse1 = `${onWall} bottle${onWallplural} of beer on the wall, ${onWall} bottle${onWallplural} of beer.`
+    let mainVerse2 = `Take ${onWall == 1 ? 'it' : 'one'} down and pass it around, ${onWall - 1 == 0 ? 'no more' : onWall - 1} bottle${onWall - 1 > 1 ? 's' : onWall - 1 == 0 ? 's' : ''} of beer on the wall.`
+
+    response.push(mainVerse1, mainVerse2);
+
+    // push blank line if not the last line
+    count != takeDownCount - 1 && response.push('');
+
+    // remove a beer from the wall (onWall) done it the for loop definition
   }
   return response;
-  // console.log(mainVerse);
-  // console.log(lastVerse);
-  // throw new Error("Remove this statement and implement this function");
 };
