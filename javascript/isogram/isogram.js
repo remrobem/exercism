7cbaa57049a1fedd7bb9ceb42e0a2251cd9b5e46
Rob Martin <rob.martin.5758@gmail.com>
2020-02-05 14:40:16 -0500

    refactored

--- javascript/isogram/isogram.js
+++ javascript/isogram/isogram.js
@@ -2,10 +2,8 @@ export const isIsogram = (isogram) => {
 
 // remove all non alpha characters  
 const string = isogram.replace(/[^A-Za-z]/g,'').toLowerCase();
-// unique values 
-const uniqueString = new Set(string);
 
 // number of unique aplpha characters must equal total number of alpha characters
 // to be an isogram
-return uniqueString.size == string.length
+return new Set(string).size == string.length
 };
