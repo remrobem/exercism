93e2adf9ffe3e6b6c8d6c29e88d6007969e0bc06
Rob Martin <rob.martin.5758@gmail.com>
2020-02-06 10:41:52 -0500

    updated comments to force change.

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -8,13 +8,14 @@ export const hey = (message) => {
     return 'Fine. Be that way!';
   }
 
-  // check for question
-  let isQuestion = compressedMessage.endsWith('?');
+  // check for question - end with ?
+  const isQuestion = compressedMessage.endsWith('?');
 
-  // check for yelling if any letters exist in the string 
+  // check for yelling  - if any letters exist, they must all be uppercase
   let isYelling = compressedMessage.search(/[A-Za-z]/) >= 0 ?
     compressedMessage.toUpperCase() == compressedMessage ? true : false
     : false;
+
 
   // return appropriate answer
   return ((isQuestion && isYelling) ? "Calm down, I know what I'm doing!"
