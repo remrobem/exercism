7fbe4b9c05164b98f4edd6943d9ccd70695cf961
Rob Martin <rob.martin.5758@gmail.com>
2020-02-06 12:38:53 -0500

    refactored

--- javascript/scrabble-score/scrabble-score.js
+++ javascript/scrabble-score/scrabble-score.js
@@ -1,32 +1,32 @@
 'use strict';
 
   const letterValues = [
-    { letter: 'A', points: 1 },
-    { letter: 'B', points: 3 },
-    { letter: 'C', points: 3 },
-    { letter: 'D', points: 2 },
-    { letter: 'E', points: 1 },
-    { letter: 'F', points: 4 },
-    { letter: 'G', points: 2 },
-    { letter: 'H', points: 4 },
-    { letter: 'I', points: 1 },
-    { letter: 'J', points: 8 },
-    { letter: 'K', points: 5 },
-    { letter: 'L', points: 1 },
-    { letter: 'M', points: 3 },
-    { letter: 'N', points: 1 },
-    { letter: 'O', points: 1 },
-    { letter: 'P', points: 3 },
-    { letter: 'Q', points: 10 },
-    { letter: 'R', points: 1 },
-    { letter: 'S', points: 1 },
-    { letter: 'T', points: 1 },
-    { letter: 'U', points: 1 },
-    { letter: 'V', points: 4 },
-    { letter: 'W', points: 4 },
-    { letter: 'X', points: 8 },
-    { letter: 'Y', points: 4 },
-    { letter: 'Z', points: 10 }
+    { A: 1 },
+    { B: 3 },
+    { C: 3 },
+    { D: 2 },
+    { E: 1 },
+    { F: 4 },
+    { G: 2 },
+    { H: 4 },
+    { I: 1 },
+    { J: 8 },
+    { K: 5 },
+    { L: 1 },
+    { M: 3 },
+    { N: 1 },
+    { O: 1 },
+    { P: 3 },
+    { Q: 10 },
+    { R: 1 },
+    { S: 1 },
+    { T: 1 },
+    { U: 1 },
+    { V: 4 },
+    { W: 4 },
+    { X: 8 },
+    { Y: 4 },
+    { Z: 10 }
   ];
 
   export const score = (word) => {
@@ -37,7 +37,5 @@
   return word
     .toUpperCase()
     .split('')
-    .reduce((score, letter) => {
-      return score += letterValues.find(entry => entry.letter == letter).points
-    }, 0)
+    .reduce((score, letter) => { score += letterValues[letter] }, 0)
 };
