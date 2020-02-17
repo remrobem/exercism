1a34c8c7904915937d8038211e9fe6310a88618f
Rob Martin <rob.martin.5758@gmail.com>
2020-02-17 17:41:00 -0500

    comments added

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,8 +1,10 @@
 export const countWords = (phrase) => {
 
+  // change to lower case and match words, including those with an '
   const words = phrase
   .toLowerCase()
-  .match(/\b[\w']+\b/g)
+  .match(/\b[\w']+\b/g);
+  // create object containing words and counts
   let counts = {};
   words.map(word => {
     return counts[word] = counts[word] ? counts[word] += 1 : 1;
