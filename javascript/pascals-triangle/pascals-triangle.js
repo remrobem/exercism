0e88c57e31dfa881f4b32303197b3f09c49dfdbc
Rob Martin <rob.martin.5758@gmail.com>
2020-03-25 16:53:27 -0400

    refactor

--- javascript/pascals-triangle/pascals-triangle.js
+++ javascript/pascals-triangle/pascals-triangle.js
@@ -1,26 +1,25 @@
 export const rows = (count) => {
 
   let triangle = [];
-  let nextRow = [1]; // initialize first row
+  let prevRow = [1]; // initialize first row
 
   for (let i = 0; i < count; i++) {
 
     // push the next row to the returned array (triangle)
-    triangle.push(nextRow);
+    triangle.push(prevRow);
 
     // format next row
     let newRow = [];
 
     // populate new (next) row from the row just added to the triangle
-    nextRow.map((num, i) => {
-      i === 0 ? newRow.push(1) : (newRow.push(num + nextRow[i - 1]));
+    prevRow.map((num, i) => {
+      i === 0 ? newRow.push(1) : (newRow.push(num + prevRow[i - 1]));
     });
 
     // add the ending 1 to the next row
     newRow.push(1);
     // save the row for the next entry into the triangle (if needed)
-    nextRow = [];
-    nextRow.push(...newRow);
+    prevRow = [...newRow]
 
   }
 
