2bab52553146c8cf15c3e1b260f635ad8d32ca66
Rob Martin <rob.martin.5758@gmail.com>
2020-07-31 20:32:00 -0400

    basic logic

--- javascript/house/house.js
+++ javascript/house/house.js
@@ -3,12 +3,68 @@
 // convenience to get you started writing code faster.
 //
 
-export class House {
+// This is the house that Jack built.
+
+// This is the malt
+// that lay in the house that Jack built.
+
+// This is the rat
+// that ate the malt
+// that lay in the house that Jack built.
+
+// This is the cat
+// that killed the rat
+// that ate the malt
+// that lay in the house that Jack built.
+
+const verseDefinitions = [
+  { noun: 'house that Jack built', verb: 'lay in' },
+  { noun: 'malt', verb: 'ate' },
+  { noun: 'rat', verb: 'killed' },
+  { noun: 'cat', verb: 'worried' },
+  { noun: 'dog', verb: 'tossed' },
+  { noun: 'cow with the crumpled horn', verb: 'milked' },
+  { noun: 'maiden all forlorn', verb: 'kissed' },
+  { noun: 'man all tattered and torn', verb: 'married' },
+  { noun: 'priest all shaven and shorn', verb: 'woke' },
+  { noun: 'rooster that crowed in the morn', verb: 'kept' },
+  { noun: 'farmer sowing his corn', verb: 'belonged' },
+  { noun: 'horse and the hound and the horn', verb: '' },
+
+];
+
+let verses = [];
+let verse = [];
+
+for (let i = 0; i < verseDefinitions.length; i++) {
+  // console.log(`i - This is the ${verseDefinitions[i].noun}`);
+  verse.push(`This is the ${verseDefinitions[i].noun}`);
+
+  for (let j = i - 1; j >= 0; j--) {
+    // console.log(
+    //   `j - that ${verseDefinitions[j].verb} the ${verseDefinitions[j].noun}`
+    // );
+    verse.push(
+      `that ${verseDefinitions[j].verb} the ${verseDefinitions[j].noun}`
+    );
+  }
+  verses.push(verse);
+  verse = [];
+}
+
+console.log(verses)
+
+class House {
+  // export class House {
+
   static verse() {
-    throw new Error("Remove this statement and implement this function");
+    // console.log(verseDefinitions[1].noun);
+    // throw new Error("Remove this statement and implement this function");
   }
 
   static verses() {
-    throw new Error("Remove this statement and implement this function");
+    // throw new Error("Remove this statement and implement this function");
   }
 }
+
+console.log(House.verse());
