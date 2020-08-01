789fe46c5d5d94135bc6f15a412444694aae7b71
Rob Martin <rob.martin.5758@gmail.com>
2020-08-01 10:56:24 -0400

    house completed
    the house challenge has been completed and pass in exercism.io

--- javascript/house/house.js
+++ javascript/house/house.js
@@ -1,70 +1,48 @@
-//
-// This is only a SKELETON file for the 'House' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-// This is the house that Jack built.
-
-// This is the malt
-// that lay in the house that Jack built.
-
-// This is the rat
-// that ate the malt
-// that lay in the house that Jack built.
-
-// This is the cat
-// that killed the rat
-// that ate the malt
-// that lay in the house that Jack built.
-
 const verseDefinitions = [
-  { noun: 'house that Jack built', verb: 'lay in' },
-  { noun: 'malt', verb: 'ate' },
-  { noun: 'rat', verb: 'killed' },
-  { noun: 'cat', verb: 'worried' },
-  { noun: 'dog', verb: 'tossed' },
-  { noun: 'cow with the crumpled horn', verb: 'milked' },
-  { noun: 'maiden all forlorn', verb: 'kissed' },
-  { noun: 'man all tattered and torn', verb: 'married' },
-  { noun: 'priest all shaven and shorn', verb: 'woke' },
-  { noun: 'rooster that crowed in the morn', verb: 'kept' },
-  { noun: 'farmer sowing his corn', verb: 'belonged' },
-  { noun: 'horse and the hound and the horn', verb: '' },
-
+  { subject: 'house that Jack built.', verb: 'lay in' },
+  { subject: 'malt', verb: 'ate' },
+  { subject: 'rat', verb: 'killed' },
+  { subject: 'cat', verb: 'worried' },
+  { subject: 'dog', verb: 'tossed' },
+  { subject: 'cow with the crumpled horn', verb: 'milked' },
+  { subject: 'maiden all forlorn', verb: 'kissed' },
+  { subject: 'man all tattered and torn', verb: 'married' },
+  { subject: 'priest all shaven and shorn', verb: 'woke' },
+  { subject: 'rooster that crowed in the morn', verb: 'kept' },
+  { subject: 'farmer sowing his corn', verb: 'belonged to' },
+  { subject: 'horse and the hound and the horn', verb: '' },
 ];
 
-let verses = [];
-let verse = [];
+export class House {
+  static getVerses = (startVerse, endVerse) => {
+    let verses = [];
+    // set start and end verses
+    startVerse -= 1;
+    endVerse = endVerse ? endVerse - 1 : startVerse;
 
-for (let i = 0; i < verseDefinitions.length; i++) {
-  // console.log(`i - This is the ${verseDefinitions[i].noun}`);
-  verse.push(`This is the ${verseDefinitions[i].noun}`);
+    // build the first line of the requested verse, 
+    // then the remainder of the lines in the verse
+    for (let i = startVerse; i <= endVerse; i++) {
+      verses.push(`This is the ${verseDefinitions[i].subject}`);
+      for (let j = i - 1; j >= 0; j--) {
+        verses.push(
+          `that ${verseDefinitions[j].verb} the ${verseDefinitions[j].subject}`
+        );
+      }
+      // add blank line after every verse except the last verse
+      if (i != endVerse) {
+        verses.push('');
+      }
+    }
+    return verses;
+  };
 
-  for (let j = i - 1; j >= 0; j--) {
-    // console.log(
-    //   `j - that ${verseDefinitions[j].verb} the ${verseDefinitions[j].noun}`
-    // );
-    verse.push(
-      `that ${verseDefinitions[j].verb} the ${verseDefinitions[j].noun}`
-    );
+  static verse(verse) {
+    return this.getVerses(verse);
   }
-  verses.push(verse);
-  verse = [];
+
+  static verses(startVerse, endVerse) {
+    return this.getVerses(startVerse, endVerse);
+
+  }
 }
-
-console.log(verses)
-
-class House {
-  // export class House {
-
-  static verse() {
-    // console.log(verseDefinitions[1].noun);
-    // throw new Error("Remove this statement and implement this function");
-  }
-
-  static verses() {
-    // throw new Error("Remove this statement and implement this function");
-  }
-}
-
-console.log(House.verse());
