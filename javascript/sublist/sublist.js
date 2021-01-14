72a7e33768bfd74767601837057860cd712734be
Rob Martin <rob.martin.5758@gmail.com>
2021-01-14 16:13:55 -0500

    refactoring

--- javascript/sublist/sublist.js
+++ javascript/sublist/sublist.js
@@ -15,27 +15,16 @@ export class List {
     const listTwo = comparelist.list ? comparelist.list.toString() : '';
 
     //EQUAL
-    if (listOne == listTwo) {
-      return EQUAL;
-    }
-
+    if (listOne == listTwo) return EQUAL;
+    
     // one of the lists is empty
-    if (!listOne.length && listTwo.length) {
-      return SUBLIST;
-    }
-    if (listOne.length && !listTwo.length) {
-      return SUPERLIST;
-    }
-
-    // SUBLIST - list 2 includes list 1
-    if (listTwo.includes(listOne)) {
-      return SUBLIST;
-    }
-
-    // SUPERLIST - list 1 includes list 2
-    if (listOne.includes(listTwo)) {
-      return SUPERLIST;
-    }
+    if (!listOne.length && listTwo.length) return SUBLIST;
+    if (listOne.length && !listTwo.length) return SUPERLIST;
+    
+    // SUBLIST 
+    if (listTwo.includes(listOne)) return SUBLIST;
+    // SUPERLIST
+    if (listOne.includes(listTwo)) return SUPERLIST;
     
     // UNEQUAL if no other matches found
     return UNEQUAL;
