3c21e3a9f93d76e907be22e6f5213f61f87bc364
Rob Martin <rob.martin.5758@gmail.com>
2021-01-29 03:43:52 -0500

    list-ops in process

---
+++ javascript/list-ops/list-ops.js
@@ -0,0 +1,52 @@
+//
+// This is only a SKELETON file for the 'List Ops' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class List {
+  constructor(values = []) {
+    // console.log({values})
+    this.values = values;
+    // throw new Error('Remove this statement and implement this function');
+  }
+
+  append(list) {
+    // console.log([...this.values, ...list])
+    return new List([...this.values, ...list.values]);
+    // throw new Error('Remove this statement and implement this function');
+  }
+
+  concat(lists) {
+    let newList = [...this.values];
+    lists.values.forEach((list) => {
+      list.values.forEach((value) => {
+        newList.push(value);
+      });
+    });
+    return new List(newList);
+  }
+
+  filter() {
+    throw new Error('Remove this statement and implement this function');
+  }
+
+  map() {
+    throw new Error('Remove this statement and implement this function');
+  }
+
+  length() {
+    throw new Error('Remove this statement and implement this function');
+  }
+
+  foldl() {
+    throw new Error('Remove this statement and implement this function');
+  }
+
+  foldr() {
+    throw new Error('Remove this statement and implement this function');
+  }
+
+  reverse() {
+    throw new Error('Remove this statement and implement this function');
+  }
+}
