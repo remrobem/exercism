afe555567dfa8aaab0c5aa5c818376de6550c671
Rob Martin <rob.martin.5758@gmail.com>
2021-06-12 23:53:44 -0400

    completed

--- javascript/resistor-color-trio/resistor-color-trio.js
+++ javascript/resistor-color-trio/resistor-color-trio.js
@@ -1,14 +1,41 @@
-//
-// This is only a SKELETON file for the 'Resistor Color Trio' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const bands = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
+];
+
+function formatOhmsText(ohms) {
+  return ohms < 1000 ? `${ohms} ohms` : `${ohms / 1000} kiloohms`;
+}
 
 export class ResistorColorTrio {
-  constructor() {
-    throw new Error('Remove this statement and implement this function');
+  constructor(colors) {
+    this.colors = colors;
+    this.label = this.label();
   }
 
   label() {
-    throw new Error('Remove this statement and implement this function');
+    // get values for the ohms and exponent
+    const [firstOhm, secondOhm, exponent] = [
+      bands.indexOf(this.colors[0]),
+      bands.indexOf(this.colors[1]),
+      bands.indexOf(this.colors[2]),
+    ];
+
+    // error if any invalid colors
+    if (exponent < 0 || firstOhm < 0 || secondOhm < 0)
+      throw new Error('invalid color');
+
+    // calulcate ohms - concat the 2 ohm numbers and multiply by the exponenet
+    let ohms = Number('' + firstOhm + secondOhm) * 10 ** exponent;
+
+    return `Resistor value: ${formatOhmsText(ohms)}`;
   }
 }
